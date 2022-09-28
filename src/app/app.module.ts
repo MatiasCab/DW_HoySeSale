af2be988f55a6ccf82534e3bbdd1045cbf70c3c7
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2022-09-28 20:56:17 -0300

    Presentation Page ready (maso)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenericButtonComponent } from './Components/generic-button/generic-button.component';
+import { AppTitleComponent } from './Components/app-title/app-title.component';
+import { PresentationPageComponent } from './Components/presentation-page/presentation-page.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GenericButtonComponent
+    GenericButtonComponent,
+    AppTitleComponent,
+    PresentationPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
