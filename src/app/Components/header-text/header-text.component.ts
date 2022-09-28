708570caa36bf7a0fe878f0491e8656e3ca9bbb5
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2022-09-28 20:39:52 -0300

    header Navbar

---
+++ src/app/Components/header-text/header-text.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-text',
+  templateUrl: './header-text.component.html',
+  styleUrls: ['./header-text.component.scss']
+})
+export class HeaderTextComponent implements OnInit {
+
+  @Input() text?: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenericButtonComponent } from './Components/generic-button/generic-button.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HeaderTextComponent } from './Components/header-text/header-text.component';
+import { ButtonIconComponent } from './Components/button-icon/button-icon.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GenericButtonComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderTextComponent,
+    ButtonIconComponent,
+    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
