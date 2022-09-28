c9396a4f712893666cbc498a6784541577f016c3
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2022-09-28 10:06:48 -0300

    Bootstrap added

---
+++ src/app/Components/generic-button/generic-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-button',
+  templateUrl: './generic-button.component.html',
+  styleUrls: ['./generic-button.component.scss']
+})
+export class GenericButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
