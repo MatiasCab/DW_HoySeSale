ee74aed8b78fd16649aa81bb81ffaffebc2eec81
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2022-10-08 17:29:45 -0300

    Entertainment page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './Components/login/login.component';
 import { SearchPageComponent } from './Components/search-page/search-page.component';
 import { PresentationPageComponent } from './Components/presentation-page/presentation-page.component';
+import { EntertainmentPageComponent } from './Components/entertainment-page/entertainment-page.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'presentation', pathMatch:"full"},
   {path:'login', component: LoginComponent, data: {animation: 'b'}},
   {path:'presentation', component: PresentationPageComponent, data: {animation: 'a'}},
-  {path:'search', component: SearchPageComponent}];
+  {path:'search', component: SearchPageComponent},
+  {path:'entertainment/:id', component: EntertainmentPageComponent}];
 
 @NgModule({
   imports: [
