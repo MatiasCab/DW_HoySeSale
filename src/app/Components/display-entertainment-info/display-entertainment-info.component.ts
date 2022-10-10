a26adc1337b389ad38b7fca05b1da2a5478ba446
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2022-10-10 11:30:19 -0300

    Pagina entretenimiento

--- src/app/Components/display-entertainment-info/display-entertainment-info.component.ts
+++ src/app/Components/display-entertainment-info/display-entertainment-info.component.ts
@@ -8,7 +8,9 @@ import { Entertainment } from 'src/app/models/entertainment';
 })
 export class DisplayEntertainmentInfoComponent implements OnInit {
 
-  @Input() entertainment!: Entertainment;
+  //@Input() entertainment!: Entertainment;
+
+  entertainment = {id: 1, name: 'VALIANT', description: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) Lorem Ipsum.', qualification: 4, profilePic: 9, gallery: [1, 2]};
 
   constructor() { }
 
