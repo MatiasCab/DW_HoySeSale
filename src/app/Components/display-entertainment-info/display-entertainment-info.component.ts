2eab1a196f6bd95c41c304b0a4319040bc1f46fa
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2022-10-15 21:48:10 -0300

    Carousel view ready

--- src/app/Components/display-entertainment-info/display-entertainment-info.component.ts
+++ src/app/Components/display-entertainment-info/display-entertainment-info.component.ts
@@ -9,6 +9,8 @@ export class DisplayEntertainmentInfoComponent implements OnInit {
 
   //@Input() entertainment!: Entertainment;
 
+  images: string[] = ["../../../assets/images/alex-voulgaris-jfoIIFLw3_Y-unsplash.jpg", "../../../assets/images/tony-pham-FUmDe-Bx1LA-unsplash.jpg"];
+
   entertainment = {id: 1, name: 'VALIANT', description: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) Lorem Ipsum.', qualification: 4, profilePic: 9, gallery: [1, 2]};
 
   constructor() { }
