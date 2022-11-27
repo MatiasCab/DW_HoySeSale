9b33c73ce17c699875a59c81a71231d3c8897a5b
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2022-11-27 03:40:02 -0300

    bugs resolved6

--- src/app/shared/Components/search-input/search-input.component.ts
+++ src/app/shared/Components/search-input/search-input.component.ts
@@ -25,7 +25,7 @@ export class SearchInputComponent implements OnInit {
   keyEnter(key: KeyboardEvent) {
     console.log(key);
     let reg = 'qwertyuiopñlkjhgfdsazxcvbnmQWERTYUIOPÑLKJHGFDSAZXCVBNM-*/1234567890!¡¿?';
-    if (reg.includes(key.key) || ('Backspace' == key.key)) {
+    if (reg.includes(key.key) || ('Backspace' == key.key) || (key.key == 'Unidentified')) { //'Unidentified' para las teclas en mobile.
 
       if (this.debounceTimer) clearTimeout(this.debounceTimer);
 
