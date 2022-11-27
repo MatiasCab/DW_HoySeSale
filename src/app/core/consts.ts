c462fc4288b67e2db4b6c071b1982bf722985b53
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2022-11-27 01:16:04 -0300

    api url change

--- src/app/core/consts.ts
+++ src/app/core/consts.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'https://hoysesale.azurewebsites.net';
+export const API_URL = 'https://hoysesale.azurewebsites.net/api/v1';
 export const TOKEN_NAME_LS = "id_token";
 export const BREAK_POINT = '(min-width: 900px)';
